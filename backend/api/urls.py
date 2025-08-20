8b5e6822f3b3c336cf94fe3777d5d82fe23eb39e
KakiGro <inakigro@gmail.com>
2025-08-19 19:20:54 -0600

    Los usuarios ahora usan un uuid en lugar de int. De preferencia recrear bases de datos

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,7 +7,7 @@ router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('usuarios/', VerUsuarios.as_view(), name='VerUsuarios'),
-    path('usuarios/<int:user_id>/', VerUsuarios.as_view(), name='usuarios-detail'),  # Para obtener un usuario por ID
+    path('usuarios/<uuid:user_id>/', VerUsuarios.as_view(), name='usuarios-detail'),  # Para obtener un usuario por ID
     path('current-user/', get_current_user, name='get_current_user'),
     path('', include(router.urls)),
 ]
--- backend/seguimiento/urls.py
+++ backend/seguimiento/urls.py
@@ -12,13 +12,13 @@ from .views import (
 urlpatterns = [
     # Apoyos
     path('guardar-seguimiento-apoyos/', GuardarSeguimientoApoyos.as_view(), name='guardar_seguimiento_apoyos'),
-    path('obtener-seguimiento-apoyos-ultimo/<int:usuario_id>/', ObtenerUltimosSeguimientosApoyos.as_view(), name='obtener_ultimo_seguimiento_apoyos'),
-    path('obtener-seguimiento-apoyos-actual/<int:usuario_id>/', ObtenerSeguimientoApoyosActual.as_view(), name='obtener_seguimiento_apoyos_actual'),
+    path('obtener-seguimiento-apoyos-ultimo/<uuid:usuario_id>/', ObtenerUltimosSeguimientosApoyos.as_view(), name='obtener_ultimo_seguimiento_apoyos'),
+    path('obtener-seguimiento-apoyos-actual/<uuid:usuario_id>/', ObtenerSeguimientoApoyosActual.as_view(), name='obtener_seguimiento_apoyos_actual'),
 
     # Proyecto de Vida
     path('guardar-seguimiento-proyecto-vida/', GuardarSeguimientoProyectoVida.as_view(), name='guardar_seguimiento_proyecto_vida'),
-    path('obtener-seguimiento-proyecto-vida-todos/<int:usuario_id>/', ObtenerSeguimientoProyectoVida.as_view(), name='obtener_todos_seguimientos_proyecto_vida'),
-    path('obtener-seguimiento-proyecto-vida-ultimo/<int:usuario_id>/', ObtenerUltimoSeguimientoProyectoVida.as_view(), name='obtener_ultimo_seguimiento_proyecto_vida'),
+    path('obtener-seguimiento-proyecto-vida-todos/<uuid:usuario_id>/', ObtenerSeguimientoProyectoVida.as_view(), name='obtener_todos_seguimientos_proyecto_vida'),
+    path('obtener-seguimiento-proyecto-vida-ultimo/<uuid:usuario_id>/', ObtenerUltimoSeguimientoProyectoVida.as_view(), name='obtener_ultimo_seguimiento_proyecto_vida'),
 
-    path('recomendaciones-apoyos/<int:usuario_id>/', recommended_technical_aids_grouped, name='recommended_aids'),
+    path('recomendaciones-apoyos/<uuid:usuario_id>/', recommended_technical_aids_grouped, name='recommended_aids'),
 ]
